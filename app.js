d7d30e566a43beb6111f98b24ed704d9bb274919
EMANUELE <65012625+Emanuele9326@users.noreply.github.com>
2021-07-19 19:38:03 +0200

    1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db= require('./database');
 require('dotenv').config();
 
 const app= express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.listen(port,()=>{
   console.log(`Starting server at ${port}`)
